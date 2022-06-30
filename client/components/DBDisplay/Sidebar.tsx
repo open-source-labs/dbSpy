f32b907af870c14b1dabcbcb28ffa8f69ffc1893
Angel Giron <angel.c.giron1@gmail.com>
2022-06-30 09:28:22 -0700

    created sidebar drawer

--- client/components/DBDisplay/Sidebar.tsx
+++ client/components/DBDisplay/Sidebar.tsx
@@ -7,8 +7,12 @@ import {
   Box,
   PasswordInput,
   Text,
+  Drawer, 
+  useMantineTheme,
+  MantineTheme
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import { Database } from 'tabler-icons-react';
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import axios from "axios";
 
@@ -36,6 +40,8 @@ export default function Sidebar({
       database_name: "",
     },
   });
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
 
   //USE QUERY FOR GET REQUEST
   //   const { data } = useQuery("initialschema");
@@ -73,54 +79,82 @@ export default function Sidebar({
   }
 
   return (
-    <Box sx={{ maxWidth: 300 }} mx="auto">
-      <form
-        onSubmit={form.onSubmit((values) => {
-          mutate(values);
-          form.setValues({
-            hostname: "",
-            username: "",
-            password: "",
-            port: "",
-            database_name: "",
-          });
-        })}
+    <>
+      <Drawer
+        position="right"
+        overlayColor={theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
+        overlayOpacity={0.55}
+        overlayBlur={3}
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Connect to database"
+        padding="xl"
+        size="md"
       >
-        <TextInput
-          required
-          label="Host"
-          //   placeholder="Host"
-          {...form.getInputProps("hostname")}
-        />
-        <TextInput
-          required
-          label="Port"
-          //   placeholder="Port"
-          {...form.getInputProps("port")}
-        />
-        <TextInput
-          required
-          label="Database Username"
-          //   placeholder="Username"
-          {...form.getInputProps("username")}
-        />
-        <PasswordInput
-          required
-          label="Database Password"
-          //   placeholder="Password"
-          {...form.getInputProps("password")}
-        />
-        <TextInput
-          required
-          label="Database Name"
-          //   placeholder="Database name"
-          {...form.getInputProps("database_name")}
-        />
+    
+        <Box sx={{ maxWidth: 300 }} mx="auto">
+          <form
+            onSubmit={form.onSubmit((values) => {
+              mutate(values);
+              form.setValues({
+                hostname: "",
+                username: "",
+                password: "",
+                port: "",
+                database_name: "",
+              });
+            })}
+          >
+            <TextInput
+              required
+              data-autofocus
+              label="Host"
+              //   placeholder="Host"
+              {...form.getInputProps("hostname")}
+            />
+            <TextInput
+              required
+              label="Port"
+              //   placeholder="Port"
+              {...form.getInputProps("port")}
+            />
+            <TextInput
+              required
+              label="Database Username"
+              //   placeholder="Username"
+              {...form.getInputProps("username")}
+            />
+            <PasswordInput
+              required
+              label="Database Password"
+              //   placeholder="Password"
+              {...form.getInputProps("password")}
+            />
+            <TextInput
+              required
+              label="Database Name"
+              //   placeholder="Database name"
+              {...form.getInputProps("database_name")}
+            />
 
-        <Group position="right" mt="md">
-          <Button type="submit">Submit</Button>
-        </Group>
-      </form>
-    </Box>
+            <Group position="right" mt="md">
+              <Button type="submit">Connect</Button>
+            </Group>
+          </form>
+        </Box>
+    </Drawer>
+
+    <Group position="right">
+      <Button 
+      //  variant="white" color="white"
+        leftIcon={<Database />}
+        styles={() => ({
+          root: {
+            marginRight: 30
+          }
+        })}
+        onClick={() => setOpened(true)}>Connect to database</Button>
+    </Group>
+    </>
   );
 }
