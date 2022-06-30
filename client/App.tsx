2bdf5826c4671e9ba84da098c62ec56c257575de
Angel Giron <angel.c.giron1@gmail.com>
2022-06-29 18:24:58 -0700

    worked on table

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Table from './Table'
 export default function App() {
 
 
@@ -7,6 +7,7 @@ export default function App() {
   return (
       <div>
           <div>Hello World</div>
+          <Table />
       </div>
   )
 }
