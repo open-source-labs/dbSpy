3d3db5461a03a62001a165de0c3775a3ad29a443
MockTech <83368864+MockTech@users.noreply.github.com>
2022-07-05 15:38:47 -0400

    Added package-lock.json to .gitignore Added dropdown functionality to client/components/DBDisplay/Table.tsx Modified formatting of client/pages/DBDisplay.tsx to be consistent with nested object datamodel Deleted server/controllers/controller.js as it was unused Modified server/routes/api.js to use dummy data on dataload

--- client/components/DBDisplay/Table.tsx
+++ client/components/DBDisplay/Table.tsx
@@ -139,16 +139,44 @@ export default function Table({ tableInfo, id }: TableProps) {
   };
 
   const columns: GridColumns = [
-    { field: "column", headerName: "Column", width: 75, editable: true },
-    { field: "type", headerName: "Type", width: 100, editable: true },
+    { 
+      field: "column", 
+      headerName: "Column", 
+      width: 75, 
+      editable: true 
+    },
+    { 
+      field: "type", 
+      headerName: "Type", 
+      width: 100, 
+      editable: true, 
+      type: "singleSelect", 
+      valueOptions: ["binary", "blob", "boolean", "date", "datetime", "decimal", "float", "integer", "serial", "string", "text", "time", "timestamp", "varchar(255)"] 
+    },
     {
       field: "constraint",
       headerName: "Constraints",
       width: 100,
       editable: true,
+      type: "singleSelect", 
+      valueOptions: ["NOT NULL", "UNIQUE"] 
     },
-    { field: "pk", headerName: "PK", width: 50, editable: true },
-    { field: "fk", headerName: "FK", width: 50, editable: true },
+    { 
+      field: "pk", 
+      headerName: "PK", 
+      width: 50, 
+      editable: true, 
+      type: "singleSelect", 
+      valueOptions: ["true", "false"] 
+    },
+    { 
+      field: "fk", 
+      headerName: "FK", 
+      width: 50, 
+      editable: true, 
+      type: "singleSelect", 
+      valueOptions: ["true", "false"] 
+    },
     {
       field: "actions",
       type: "actions",
