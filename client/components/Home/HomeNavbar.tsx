36969cb07b52899ef36209d9c90c36795401056c
Kevin Park-Lee <kevin38424@gmail.com>
2022-07-15 19:26:15 -0400

    commit

--- client/components/Home/HomeNavbar.tsx
+++ client/components/Home/HomeNavbar.tsx
@@ -1,42 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 export default function HomeNavbar() {
   return (
-      <Box sx={{flexGrow: 1}}>
-        <AppBar position="static" sx={{bgcolor: '#2b3a42'}}>
-          <Toolbar>
-           
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Logo dbSpy
-            </Typography>
-            <Link style={{textDecoration: 'none', color: "white" }} to={"/login"} >
-              <Button color="inherit">Free Demo</Button>
-              
-            </Link>
-            <Button color="inherit">Docs</Button>
-            
-            <Button color="inherit">Team</Button>
-            
-            <Link style={{textDecoration: 'none', color: "white" }} to={"/signup"}>
-              
-              <Button color="inherit">Sign Up</Button>
-            </Link>
-            
-            <Link style={{textDecoration: 'none', color: "white" }} to={"/login"}>
-              <Button color="inherit">Log In</Button>
-            </Link>
-            
-          </Toolbar>
-        </AppBar>
-      </Box>
-    
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ bgcolor: "#2b3a42" }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Logo dbSpy
+          </Typography>
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to={"/login"}
+          >
+            <Button color="inherit">Free Demo</Button>
+          </Link>
+          <Button color="inherit">Docs</Button>
+
+          <Button color="inherit">Team</Button>
+
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to={"/signup"}
+          >
+            <Button color="inherit">Sign Up</Button>
+          </Link>
+
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to={"/login"}
+          >
+            <Button color="inherit">Log In</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
