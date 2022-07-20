fd8f58942141fa4edc830cdf04c5f1da42f8bb14
Kevin Park-Lee <kevin38424@gmail.com>
2022-07-19 20:20:28 -0400

    hosting with Netlify

--- client/pages/Signup.tsx
+++ client/pages/Signup.tsx
@@ -60,11 +60,7 @@ export default function Signup() {
   const google = () => {
     const strWindowFeatures =
       'toolbar=no, menubar=no, width=600, height=700, top=100, left=100';
-    window.open(
-      'http://localhost:3000/auth/google',
-      '_self',
-      strWindowFeatures
-    );
+    window.open('http://dbspy.net/auth/google', '_self', strWindowFeatures);
   };
 
   /*
