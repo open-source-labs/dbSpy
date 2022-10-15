82a8bef302009b219f504bd4628b29f9e985c09b
Kris Magat <kmag.dev@gmail.com>
2022-10-15 14:49:42 -0700

    Co-authored-by: KrisMagat<KrisMagat@users.noreply.github.com> Co-authored-by: kwang929 <kwang929@users.noreply.github.com> Co-authored-by: Santiago Gil <santigilma@gmail.com> Co-authored-by: adziu1234 <adziu1234 @users.noreply.github.com> Co-authored-by:AnthonyAl-Rifai<AnthonyAl-Rifai@users.noreply.github.com>

---
+++ client/store/userStore.js
@@ -0,0 +1,20 @@
+import create from 'zustand';
+
+const useCredentialsStore = create((set) => ({
+  user: null,
+  //username is a string
+  setUser: (username) => set((state) => ({ ...state, user: username })),
+
+  dbCredentials: {
+    database_name: null,
+    username: null,
+    password: null,
+    host: null,
+    port: null,
+  },
+  //dbFormInput is an object
+  setDbCredentials: (dbFormInput) =>
+    set((state) => ({ ...state, dbFormInput })),
+}));
+
+export default useCredentialsStore;
