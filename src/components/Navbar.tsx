13aca6fbe53647ae92031089e216e3104e189a1b
Santiago <santigilma@gmail.com>
2022-10-15 15:10:04 -0400

    change color of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const linkbtn = "block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-whi
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-blue-500 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-sky-800 p-6">
       <div className='text-base lg:flex-grow'>
         <img className="fill-current mr-2 lg:inline-block" src={logo}/>
         <NavLink to='/' className={linkbtn}>Home</NavLink>
