3bf844d7b6c6ca507cd8b3d2a44090b5f187029f
Adrian <adrianwreczek@gmail.com>
2022-10-17 14:32:25 -0500

    removed unused legacy home components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 // React & React Router Modules
 import React from "react";
+import {NavLink} from 'react-router-dom';
 
 //Components imported
 import Body from "../components/Home/Body";
@@ -46,7 +47,7 @@ export default function Home() {
         <div className="block rounded-lg shadow-lg px-6 py-12 md:py-16 md:px-12 dark:bg-slate-900 heroCard">
           <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Database development <br /><span className="text-blue-600">simplified.</span></h1> 
           <br />
-          <a className="inline-block px-7 py-3 mb-2 md:mb-0 mr-0 md:mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" href="#!" role="button">Get started</a>
+          <NavLink to='/signup' className="inline-block px-7 py-3 mb-2 md:mb-0 mr-0 md:mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light" role="button">Get Started</NavLink>
         </div>
       </div>
     </div>
