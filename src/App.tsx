94ae90bfcd55f52b59ff8db9e8fa4c48a7ad2ca4
Santiago <santigilma@gmail.com>
2022-10-17 12:58:40 -0400

    fixed routing to /display.

--- src/App.tsx
+++ src/App.tsx
@@ -6,48 +6,49 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import  "./styles/index.css"
 import Shared from "./pages/Shared";
-
-
+// import { Dashboard } from "tabler-icons-react";
+import ProtectedRoute from "./pages/ProtectedRoute";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   /*
   States Declaration:
   "user" - object that describes the user data coming back from OAuth2.0;
   */
-  const [user, setUser] = useState({
-    name: null,
-    email: null,
-    id: null,
-    picture: null,
-  });
+  // const [user, setUser] = useState({
+  //   name: null,
+  //   email: null,
+  //   id: null,
+  //   picture: null,
+  // });
 
   /*
   UseEffect to fetch user data from "/protected" route;
   "fetchData" - a function that fetches user data from "/protected" route;
   setUser with data come back from fetch request;
   */
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetch("/protected");
-      const result = await data.json();
-      if (result !== null) {
-        setUser({
-          name: result.given_name,
-          email: result.email,
-          id: result.id,
-          picture: result.picture,
-        });
-      } else {
-        setUser({ name: null, email: null, id: null, picture: null });
-      }
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await fetch("/protected");
+  //     const result = await data.json();
+  //     if (result !== null) {
+  //       setUser({
+  //         name: result.given_name,
+  //         email: result.email,
+  //         id: result.id,
+  //         picture: result.picture,
+  //       });
+  //     } else {
+  //       setUser({ name: null, email: null, id: null, picture: null });
+  //     }
+  //   };
 
-    /* invocation of "fetchData" with error handling with catch */
-    fetchData().catch((err) => {
-      console.error;
-      setUser({ name: null, email: null, id: null, picture: null });
-    });
-  }, []);
+  //   /* invocation of "fetchData" with error handling with catch */
+  //   fetchData().catch((err) => {
+  //     console.error;
+  //     setUser({ name: null, email: null, id: null, picture: null });
+  //   });
+  // }, []);
 
   /*
     React Router, a library for Client-Side Rendering, with 4 different paths:
@@ -56,14 +57,22 @@ export default function App() {
     3. "/login" - login page
     4. "/display" | "/display/:id" - database visualization application page; only accessible when user is authorized;
   */
+
+  const user = null;
+
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Shared/>}>
       {/* index renders root directory */}
-      <Route index element={<Home user={user} setUser={setUser} />}/>
+      {/* <Route index element={<Home user={user} setUser={setUser} />}/> */}
       <Route path='login' element={<Login/>}/>
       <Route path='signup' element={<Signup/>}/>
+      <Route path='display' element={
+        <ProtectedRoute user={user}>
+          <Dashboard/>
+        </ProtectedRoute>
+      }/>
       </Route>
     </Routes>
     </BrowserRouter>
