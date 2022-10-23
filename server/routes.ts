91ea2a415af89a33624d44253509b7b55a935ec0
Santiago <santigilma@gmail.com>
2022-10-23 01:33:37 -0400

    add custom logger, start migration to server.js to TypeScript. Handle routes through routes.ts

---
+++ server/routes.ts
@@ -0,0 +1,13 @@
+import { Express, Request, Response } from 'express'
+import { handleGoogleAuth } from './controllers/sessionsController'
+// import { getAuthenticatedClient, handleOAuthCallBack } from './controllers/googleAuth';
+
+function routes(app: Express) {
+    app.get('/api/healthcheck', (req, res) => res.sendStatus(200))
+
+    // app.get('/api/me/oauth', getAuthenticatedClient, (req, res) => res.sendStatus(200))
+    // app.get('/api/oauth/google', handleOAuthCallBack, (req, res) => res.sendStatus(200))
+    app.get('/api/oauth/google', handleGoogleAuth)
+}
+
+export default routes;
