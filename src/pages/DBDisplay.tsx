68d1bd1b880d722929810e4d384536a108511425
Kris Magat <kmag.dev@gmail.com>
2022-10-22 17:13:13 -0700

    added modals to each action button.

--- src/pages/DBDisplay.tsx
+++ src/pages/DBDisplay.tsx
@@ -19,33 +19,47 @@ const DBDisplay = () => {
 
   
   
-  //create references for HTML elemends
+  //create references for HTML elements
   const mySideBarId:any = useRef();
   const mainId:any = useRef();
+  const testBtn:any = useRef();
+  const confirmModal:any = useRef();
+
+
   /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-  function openNav() {
+  const openNav = () => {
     mySideBarId.current.style.width = "400px";
     mainId.current.style.marginRight = "400px";
   }
 
+
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  function closeNav() {
+  const closeNav = () => {
     mySideBarId.current.style.width = "0";
-    mainId.current.style.marginRight = "50px";
+    mainId.current.style.marginRight = "50px"
   }
+  
 /* Sidebar handler*/
   function handleSidebar(){
-    if (sidebarDisplayState){
-      closeNav();
-    } else {
-      openNav();
-    }
+    if (sidebarDisplayState) closeNav()
+    else openNav();
   }
+
+  /* When the user clicks, open the modal */
+  const openModal = () => {
+    confirmModal.current.style.display = "block";
+  }
+
+  /* When the user clicks 'yes' or 'no', close it */
+  const closeModal = () => {
+    confirmModal.current.style.display = "none";
+  }
+
 
   return (
     <div id='DBDisplay'>
       <div ref={mySideBarId} id="mySidenav" className="sidenav">
-        <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
+        <a href="#" className="closebtn" onClick={closeNav}>&times;</a>
         <Sidebar closeNav={closeNav} />
       </div>
 
@@ -54,12 +68,14 @@ const DBDisplay = () => {
 
       {/* <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page --> */}
       <div ref={mainId} id="main">
-        {welcome ? <div className="canvas-ConnectToDatabase dark:text-[#f8f4eb] transition-colors duration-500">
-          <h3>Welcome to dbSpy!</h3>
-          Please connect your database, upload a SQL file, or build your
-          database from scratch!
-        </div>     
-        : <Flow />
+        {welcome ? 
+          <div className="canvas-ConnectToDatabase dark:text-[#f8f4eb] transition-colors duration-500">
+            <h3>Welcome to dbSpy!</h3>
+            <p>Please connect your database, upload a SQL file, or build your
+            database from scratch!
+            </p>
+          </div>     
+          : <Flow />
         }
       </div>
     </div> 
