821eff0e892d2ecd3229c2d5de33d0d79520aac5
Santiago <santigilma@gmail.com>
2022-10-25 22:17:26 -0400

    further cleanup

--- server/routes.ts
+++ server/routes.ts
@@ -37,8 +37,6 @@ const routes = async (app: Express) => {
         }
     }))
 
-    app.get('/api/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
-
     app.get('/api/oauth/google', handleGoogleAuth)
 
     app.use('/api/sql/postgres', router)
