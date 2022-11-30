fade9d6d962bab96990aef8245704309374a2270
atu816 <alexandertu95@gmail.com>
2022-11-30 09:29:02 -0800

    Added test file. Composite keys don't pass.

--- tests/util/parse.spec.js
+++ tests/util/parse.spec.js
@@ -5,17 +5,16 @@ import path from 'path';
 describe('Parsing SQL Text Data', () => {
   const testTableQuery = `
       CREATE TABLE accounts (
-      user_id serial PRIMARY KEY,
+      user_id integer PRIMARY KEY,
       username VARCHAR ( 50 ) UNIQUE NOT NULL,
       password VARCHAR ( 50 ) NOT NULL,
       email VARCHAR ( 255 ) UNIQUE NOT NULL,
       created_on TIMESTAMP NOT NULL,
       last_login TIMESTAMP,
-      FOREIGN KEY (user_id) REFERENCES accounts2(user_id)
       );
       
       CREATE TABLE accounts2 (
-        user_id serial PRIMARY KEY,
+        user_id integer PRIMARY KEY,
         username VARCHAR ( 50 ) UNIQUE NOT NULL,
         password VARCHAR ( 50 ) NOT NULL,
         email VARCHAR ( 255 ) UNIQUE NOT NULL,
@@ -25,24 +24,25 @@ describe('Parsing SQL Text Data', () => {
       `;
 
   const testFile = fs.readFileSync(path.join(__dirname, 'test_db.sql')).toString();
-  // console.log(testFile);
+  console.log(testFile);
 
-  let test = parseSql(testTableQuery);
-  console.log(test);
+  let test_var = parseSql(testTableQuery);
+  let test_db = parseSql(testFile);
+  console.log('test_db parsed', test_db)
   it('should have proper number of tables', () => {
-    const tables = testTableQuery.match(/CREATE TABLE/g);
-    const parsedTables = Object.keys(test).length;
+    const tables = testFile.match(/CREATE TABLE/g);
+    const parsedTables = Object.keys(test_db).length;
     expect(parsedTables).toBe(tables.length)
   })
 
   it('should have proper number of columns per table', () => {
-    const tableColumns = Object.keys(test.accounts).length;
+    const tableColumns = Object.keys(test_db.accounts).length;
     expect(tableColumns).toBe(6)
   })
 
   it('should properly identify PK', () => {
-    const pkCount = testTableQuery.match(/PRIMARY KEY/g).length;
-    const parsedKeys = Object.values(test).reduce((pk, table) => {
+    const pkCount = testFile.match(/PRIMARY KEY/g).length;
+    const parsedKeys = Object.values(test_db).reduce((pk, table) => {
       Object.values(table).forEach(column => {
         if (column.IsPrimaryKey) pk += 1;
       })
--- tests/util/test_db.sql
+++ tests/util/test_db.sql
@@ -31,9 +31,7 @@ CREATE TABLE production.products (
 	brand_id INT NOT NULL,
 	category_id INT NOT NULL,
 	model_year SMALLINT NOT NULL,
-	list_price DECIMAL (10, 2) NOT NULL,
-	FOREIGN KEY (category_id) REFERENCES production.categories (category_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (brand_id) REFERENCES production.brands (brand_id) ON DELETE CASCADE ON UPDATE CASCADE
+	list_price DECIMAL (10, 2) NOT NULL
 );
 
 CREATE TABLE sales.customers (
@@ -67,9 +65,7 @@ CREATE TABLE sales.staffs (
 	phone VARCHAR (25),
 	active tinyint NOT NULL,
 	store_id INT NOT NULL,
-	manager_id INT,
-	FOREIGN KEY (store_id) REFERENCES sales.stores (store_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (manager_id) REFERENCES sales.staffs (staff_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+	manager_id INT
 );
 
 CREATE TABLE sales.orders (
@@ -81,10 +77,7 @@ CREATE TABLE sales.orders (
 	required_date DATE NOT NULL,
 	shipped_date DATE,
 	store_id INT NOT NULL,
-	staff_id INT NOT NULL,
-	FOREIGN KEY (customer_id) REFERENCES sales.customers (customer_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (store_id) REFERENCES sales.stores (store_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (staff_id) REFERENCES sales.staffs (staff_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+	staff_id INT NOT NULL
 );
 
 CREATE TABLE sales.order_items (
@@ -94,16 +87,12 @@ CREATE TABLE sales.order_items (
 	quantity INT NOT NULL,
 	list_price DECIMAL (10, 2) NOT NULL,
 	discount DECIMAL (4, 2) NOT NULL DEFAULT 0,
-	PRIMARY KEY (order_id, item_id),
-	FOREIGN KEY (order_id) REFERENCES sales.orders (order_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (product_id) REFERENCES production.products (product_id) ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY (order_id, item_id)
 );
 
 CREATE TABLE production.stocks (
 	store_id INT,
 	product_id INT,
 	quantity INT,
-	PRIMARY KEY (store_id, product_id),
-	FOREIGN KEY (store_id) REFERENCES sales.stores (store_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (product_id) REFERENCES production.products (product_id) ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY (store_id, product_id)
 );
