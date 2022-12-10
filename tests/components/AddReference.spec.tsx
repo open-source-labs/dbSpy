1ab3c16c44d1a7abc48dba8e2fd270ca8c4666d0
neighbor-peace <mcostello91@gmail.com>
2022-12-09 16:00:41 -0800

    init AddReference.spec.test and match snapshot
    Co-authored-by: Michael Costello <86714629+neighbor-peace@users.noreply.github.com>
    Co-authored-by: Steven Geiger <73615329+geistnine@users.noreply.github.com>
    Co-authored-by: Yufa Li <112290188+01001101CK@users.noreply.github.com>
    Co-authored-by: Alexander Tu <110681267+atu816@users.noreply.github.com>

---
+++ tests/components/AddReference.spec.tsx
@@ -0,0 +1,44 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import AddReference from '../../src/components/DBDisplay/AddReference';
+
+// schemaStore mock
+jest.mock('../../src/store/schemaStore', () => ({
+  __esModule: true,
+  default: () => ({
+    schemaStore: {
+      table1: {
+        column1: {
+          References: [{}],
+        },
+      },
+      table2: {
+        column2: {
+          References: [{}],
+        },
+      },
+      addForeignKeySchema: jest.fn(),
+    },
+  }),
+}));
+
+// settingsStore mock
+jest.mock('../../src/store/settingsStore', () => ({
+  __esModule: true,
+  default: () => ({
+    currentTable: 'table1',
+    currentColumn: 'column1',
+    setEditRefMode: () => null,
+  }),
+}));
+
+describe('AddReference', () => {
+  beforeAll(() => {
+    render(<AddReference />);
+  });
+
+  it('matches the snapshot', () => {
+    const addRefComponent = document.querySelector('#addReference');
+    expect(addRefComponent).toMatchSnapshot();
+  });
+});
