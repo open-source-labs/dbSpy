8d711c33809d91e08999b5346ad8f3c52f1fdde7
geistnine <sgeiger5516@gmail.com>
2022-12-13 15:44:27 -0500

    added path to server routes
    Co-authored-by: Michael Costello <86714629+neighbor-peace@users.noreply.github.com>
    Co-authored-by: Steven Geiger <73615329+geistnine@users.noreply.github.com>
    Co-authored-by: Yufa Li <112290188+01001101CK@users.noreply.github.com>
    Co-authored-by: Alexander Tu <110681267+atu816@users.noreply.github.com>

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -14,6 +14,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 import Redis from 'ioredis'
 import { getCurrentUser } from '../service/session.service'
+import path from 'path';
 
 
 const client_url = process.env.NODE_ENV === 'development' ? process.env.DEV_CLIENT_ENDPOINT : process.env.CLIENT_ENDPOINT
