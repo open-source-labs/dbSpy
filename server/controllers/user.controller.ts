dd90cf8a260ca48d86014120eb897d3afdd0c15d
neighbor-peace <mcostello91@gmail.com>
2023-02-21 13:42:20 -0800

    Add logs to user controller

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { RequestHandler } from "express"
+import { RequestHandler } from 'express';
 import log from '../logger/index';
 import mysql from 'mysql2';
 import bcrypt from 'bcrypt';
@@ -8,41 +8,45 @@ dotenv.config();
 
 // Find user via Email
 export const findUser = async (email: string, password?: string) => {
+  log.info('Finding user (helper function)');
   const connection = mysql.createConnection(process.env.DATABASE_URL!);
-  const queryStr = 'SELECT * FROM users WHERE email = ?'
+  const queryStr = 'SELECT * FROM users WHERE email = ?';
   return new Promise((resolve, reject) => {
     connection.query(queryStr, [email], (err, rows) => {
-      if (err) return reject(err)
+      if (err) return reject(err);
       if (Array.isArray(rows) && rows.length === 0) return resolve(false);
-      else return resolve(rows)
-    })
-  })
-
-}
+      else return resolve(rows);
+    });
+  });
+};
 
 // Creating user via Google OAuth
 export const createUser = async (user: string[]) => {
+  log.info('Creating user (helper function)');
   const connection = mysql.createConnection(process.env.DATABASE_URL!);
-  const queryStr = 'INSERT IGNORE INTO users (sub, full_name, email, picture) VALUES (?, ?, ?, ?)';
+  const queryStr =
+    'INSERT IGNORE INTO users (sub, full_name, email, picture) VALUES (?, ?, ?, ?)';
   const values = user;
   connection.query(queryStr, values, (err, rows) => {
     if (err) {
-      log.info(err.message)
-      throw new Error('Error on createUser: Could not create new user on DB.')
+      log.info(err.message);
+      throw new Error('Error on createUser: Could not create new user on DB.');
     }
-    log.info('createUser: successfully created User')
-  })
-}
+    log.info('createUser: successfully created User');
+  });
+};
 
 // Register w/o OAuth
 export const userRegistration: RequestHandler = async (req, res, next) => {
+  log.info('Registering user (middleware)');
   const connection = mysql.createConnection(process.env.DATABASE_URL!);
-  const foundUser = await findUser(req.body.email)
+  const foundUser = await findUser(req.body.email);
   if (!foundUser) {
     const { full_name, email, password } = req.body;
     bcrypt.hash(password, saltRounds, (err, hashedPW) => {
-      if (err) return next(err)
-      const queryStr = 'INSERT IGNORE INTO users (full_name, email, password) VALUES (?, ?, ?)';
+      if (err) return next(err);
+      const queryStr =
+        'INSERT IGNORE INTO users (full_name, email, password) VALUES (?, ?, ?)';
       const values = [full_name, email, hashedPW];
       connection.query(queryStr, values, (err, data) => {
         if (err) {
@@ -52,55 +56,55 @@ export const userRegistration: RequestHandler = async (req, res, next) => {
           log.info(`${email} succesfully registered!`);
           return res.redirect(200, `/`);
         }
-      })
-    })
-  }
-  else return res.status(403).json({ err: 'Email already in use' });
-}
+      });
+    });
+  } else return res.status(403).json({ err: 'Email already in use' });
+};
 
 export const verifyUser: RequestHandler = async (req, res, next) => {
-  const foundUser = await findUser(req.body.email) as Array<{ password: string }>;
+  log.info('Verifying user (middleware)');
+  const foundUser = (await findUser(req.body.email)) as Array<{ password: string }>;
   if (foundUser) {
-    const hashedPW = foundUser[0].password
+    const hashedPW = foundUser[0].password;
     if (hashedPW === null) {
       log.info('Email Address exists inside OAuth');
       return res.redirect(403, '/login');
     }
-    bcrypt.compare(req.body.password, hashedPW)
-      .then(match => {
-        if (!match) {
-          log.info('Incorrect password')
-          return res.redirect(403, '/login');
-        }
-        else {
-          log.info('Username/Password confirmed');
-          res.locals.user = foundUser[0];
-          return res.status(200).json(res.locals.user);
-        }
-      })
+    bcrypt.compare(req.body.password, hashedPW).then((match) => {
+      if (!match) {
+        log.info('Incorrect password');
+        return res.redirect(403, '/login');
+      } else {
+        log.info('Username/Password confirmed');
+        res.locals.user = foundUser[0];
+        return res.status(200).json(res.locals.user);
+      }
+    });
   } else {
-    return res.status(403).json({ err: 'Email address not found' })
+    return res.status(403).json({ err: 'Email address not found' });
   }
-}
+};
 
 // Save currentSchema into database
 export const saveSchema: RequestHandler = async (req, res, next) => {
+  log.info(`Saving user's schema (middleware)`);
   const connection = mysql.createConnection(process.env.DATABASE_URL!);
-  const updateColQuery = `UPDATE users SET pg_schema = '${req.body.schema}' WHERE email = '${req.body.email}';`
+  const updateColQuery = `UPDATE users SET pg_schema = '${req.body.schema}' WHERE email = '${req.body.email}';`;
   connection.query(updateColQuery, (err, data) => {
     if (err) return next(err);
     log.info('Updated', data);
-    return res.status(200)
-  })
-}
+    return res.status(200);
+  });
+};
 
 // Retrive saved schema
 export const retrieveSchema: RequestHandler = async (req, res, next) => {
+  log.info(`Retrieving saved user's saved schema (middleware)`);
   const connection = mysql.createConnection(process.env.DATABASE_URL!);
-  const updateColQuery = `SELECT pg_schema FROM users WHERE email = '${req.params.email}';`
+  const updateColQuery = `SELECT pg_schema FROM users WHERE email = '${req.params.email}';`;
   connection.query(updateColQuery, (err, data) => {
-    if (err) return next(err)
+    if (err) return next(err);
     const schema = data as Array<{ pg_schema: string }>;
-    return res.status(200).json(schema[0].pg_schema)
-  })
-}
+    return res.status(200).json(schema[0].pg_schema);
+  });
+};
