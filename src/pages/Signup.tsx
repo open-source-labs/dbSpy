bd5d9a7e2f5d72ee411bac2c23e5e1f8f46810e5
neighbor-peace <mcostello91@gmail.com>
2023-02-21 19:59:47 -0800

    disable OAuth

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -125,15 +125,16 @@ export default function Signup() {
             </button>
           </div>
         </form>
-        <div className="inline-flex w-full items-center justify-between">
+        {/* TODO: Implement production scale OAuth */}
+        {/* <div className="inline-flex w-full items-center justify-between">
           <hr className="my-8 h-px w-32 border-0 bg-gray-200 dark:bg-gray-700"></hr>
           <span className="absolute left-1/2 -translate-x-1/2 px-3 font-medium text-gray-900 dark:bg-slate-700 dark:text-white">
             or
           </span>
           <hr className="my-8 h-px w-32 border-0 bg-gray-200 dark:bg-gray-700"></hr>
-        </div>
+        </div> */}
       </div>
-      <button
+      {/* <button
         className="inline-flex items-center bg-red-600 py-1 px-4 text-stone-100 hover:bg-red-700"
         onClick={handleOAuthLogin}
       >
@@ -153,7 +154,7 @@ export default function Signup() {
           <path d="M17.788 5.108A9 9 0 1021 12h-8" />
         </svg>
         <span>Sign up with Google</span>
-      </button>
+      </button> */}
     </div>
   );
 }
