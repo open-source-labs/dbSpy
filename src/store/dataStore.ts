a0385a1c4cf57ad00cfbd1862b688540dfd40172
Das <dahae0309@hotmail.com>
2023-05-11 13:37:35 -0500

    working on cleaning up dataStore file

--- src/store/dataStore.ts
+++ src/store/dataStore.ts
@@ -1,5 +1,3 @@
-//need to finish building this file#####################
-
 import { DataStore } from '@/Types';
 import create from 'zustand';
 import { devtools, subscribeWithSelector } from 'zustand/middleware';
@@ -10,22 +8,22 @@ export type DataState = {
   // DATA
   dataStore: DataStore;
   system: 'PostgreSQL' | 'MySQL' | 'Microsoft SQL' | 'Oracle SQL';
-  history: DataStore[];
-  historyCounter: number;
+  // history: DataStore[];
+  // historyCounter: number;
 
   // DATA SETTERS
   setDataStore: (dataInfo: DataStore) => void;
-  addTableData: (tableName: string, rowDataArr: any) => void;
-        addRow: (tableName: string, rowDataArr: any) => void,
-    _addHistory: (newState: any) => void;
+  // addTableData: (tableName: string, rowDataArr: any) => void;
+    //     addRow: (tableName: string, rowDataArr: any) => void,
+    // _addHistory: (newState: any) => void;
   setSystem: (system: DataStore) => void;
 
   // _checkTableValidity: (tableName: string, rowDataArr?: DataRowArray[]) => void;
-    _addRows: (
-    newStore: DataStore,
-    tableName: string,
-    columnDataArr: any
-  ) => DataStore;
+  //   _addRows: (
+  //   newStore: DataStore,
+  //   tableName: string,
+  //   columnDataArr: any
+  // ) => DataStore;
 }
 
 const useDataStore = create<DataState>()(
@@ -41,59 +39,59 @@ const useDataStore = create<DataState>()(
         historyCounter: 0,
         setSystem: (system) => set((state) => ({ ...state, system })),
         setDataStore: (dataInfo) => set((state) => ({ ...state, dataStore: dataInfo })),
-        addRow: (tableName, rowDataArr) =>
-          set((state) => {
-            console.log('are we in here??? dataStore',state)
-            const newState = { ...state };
-            newState.dataStore = get()._addRows(
-              newState.dataStore,
-              tableName,
-              rowDataArr
-            );
-            get()._addHistory(newState);
-            return newState;
-          }),
-        _addHistory: (newState) => {
-          // newState.historyCounter += 1;
-          // newState.history[newState.historyCounter] = JSON.parse(  //ERROR!!!
-          //   JSON.stringify(newState.dataStore)
-          // );
-          // if (newState.history[newState.historyCounter + 1]) {
-          //   newState.history = newState.history.slice(0, newState.historyCounter + 1);
-          // }
-          console.log(newState)
-        },
-        addTableData: (tableName, rowDataArr) => {
-          set((state) => {
-            // Check data validity first. If invalid, error is thrown
-            // get()._checkTableValidity(tableName, rowDataArr);
-            const newState = {
-              ...state,
-              dataStore: {
-                ...state.dataStore,
-                [tableName]: {},
-              },
-            };
-            newState.dataStore = get()._addRows(
-              newState.dataStore,
-              tableName,
-              rowDataArr
-            );
-            get()._addHistory(newState);
-            return newState;
-          });
-        },
-        _addRows: (newStore, tableName, rowDataArr) => {
-          const newRow: rowData = rowDataArr
-            // reassigning newStore so subscriptions pick up on the change
-          newStore = {
-            ...newStore,
-            [tableName]: {
-              ...newStore[tableName],
-              newRow,
-            },
-          };
-        }
+        // addRow: (tableName, rowDataArr) =>
+        //   set((state) => {
+        //     console.log('are we in here??? dataStore',state)
+        //     const newState = { ...state };
+        //     newState.dataStore = get()._addRows(
+        //       newState.dataStore,
+        //       tableName,
+        //       rowDataArr
+        //     );
+        //     get()._addHistory(newState);
+        //     return newState;
+        //   }),
+        // _addHistory: (newState) => {
+        //   // newState.historyCounter += 1;
+        //   // newState.history[newState.historyCounter] = JSON.parse(  //ERROR!!!
+        //   //   JSON.stringify(newState.dataStore)
+        //   // );
+        //   // if (newState.history[newState.historyCounter + 1]) {
+        //   //   newState.history = newState.history.slice(0, newState.historyCounter + 1);
+        //   // }
+        //   console.log(newState)
+        // },
+        // addTableData: (tableName, rowDataArr) => {
+        //   set((state) => {
+        //     // Check data validity first. If invalid, error is thrown
+        //     // get()._checkTableValidity(tableName, rowDataArr);
+        //     const newState = {
+        //       ...state,
+        //       dataStore: {
+        //         ...state.dataStore,
+        //         [tableName]: {},
+        //       },
+        //     };
+        //     newState.dataStore = get()._addRows(
+        //       newState.dataStore,
+        //       tableName,
+        //       rowDataArr
+        //     );
+        //     // get()._addHistory(newState);
+        //     return newState;
+        //   });
+        // },
+        // _addRows: (newStore, tableName, rowDataArr) => {
+        //   const newRow: rowData = rowDataArr
+        //     // reassigning newStore so subscriptions pick up on the change
+        //   newStore = {
+        //     ...newStore,
+        //     [tableName]: {
+        //       ...newStore[tableName],
+        //       newRow,
+        //     },
+        //   };
+        // }
           
         
       })
