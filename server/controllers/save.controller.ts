8d4f789d7fecb3fd140662e152366c876fa271f7
Donald-Macak <don.mack83@ymail.com>
2023-12-18 18:27:22 -0600

    saveRouterv1.3

--- server/controllers/save.controller.ts
+++ server/controllers/save.controller.ts
@@ -28,81 +28,97 @@ interface GlobalError {
   message: string | { err: string };
 }
 
-/*  const { email, SaveName, schema, DataType } = req.body;
-    console.log(email, SaveName, schema, DataType);
-    try {
-      const user = (await pool.query(
-        `SELECT * FROM users WHERE email = ?;`,
-        email
-      )) as RowDataPacket[];
-
-      console.log('=======');
-      console.log('user[0][0]');
-      console.log('=======');
-      // i just discovered an issue where multiple users are being creating
-      console.log(user[0][0]); */
-
-//const foundUser = (await findUser(email)) as RowDataPacket[][];
-
 const saveController = {
   // clone an existing save
   clone: async (req: Request, res: Response, next: NextFunction) => {
     log.info('[saveCtrl - cloneSchema] Begining cloneing process');
-    // will need to request current database selected from front end (by email/name)
-    // will find then create a new instance of the database with the string = string + _new as name
-    //include an initial error for no database selected
+    //necesary information to complete request
+    const { email, SaveName } = req.body;
+    //type checking
+    if (typeof email !== 'string' || typeof SaveName !== 'string') {
+      next({
+        log: 'input type was not a string',
+        message: 'invalid inputs',
+        status: 400,
+      });
+    }
+    const newName: string = SaveName + '_new';
+    try {
+      //querys
+      //first we need to retrieve the schema
+      //second we create a new table entry with that schema data as an arguement
+      const getSchemaQuery: string =
+        'SELECT SaveData FROM saveddb WHERE email = ? AND SaveName = ?';
+      const getSchemaValues = [email, SaveName];
+      const getSchema = (await pool.query(
+        getSchemaQuery,
+        getSchemaValues
+      )) as RowDataPacket[];
+      //grabbing the data off of the SaveData of the current Object
+      const schema = getSchema[0][0].SaveData;
+      const type = getSchema[0][0].DataType;
+      // create
+      const createCloneQuery =
+        'INSERT INTO saveddb(email,SaveName,DataType,SaveData) VALUES (?,?,?,?)';
+      const createCloneValues = [email, newName, type, schema];
+      const newEntry = await pool.query(createCloneQuery, createCloneValues);
+      console.log(newEntry);
+      res.locals.message = 'new Entry Succesfully created';
 
-    return next();
+      log.info('[saveCtrl - cloneSchema] Queries to database did not throw an error');
+      return next();
+    } catch (err) {
+      return next({
+        log: 'mySQL query failed inside saveController.clone',
+        message: 'something went wrong in the clone process',
+        status: 400,
+      });
+    }
   },
 
   //save over currently selected save
   save: async (req: Request, res: Response, next: NextFunction) => {
     log.info("[saveCtrl - saveSchema] Begining to save user's schema...");
     //const {email} = req.session; // this will change to session when live
-    const { email, SaveName, schema, DataType } = req.body;
-    console.log(email, SaveName, schema, DataType);
+    const { email, SaveName, schema } = req.body;
+    console.log(email, SaveName, schema);
     try {
-      const user = (await pool.query(
-        `SELECT * FROM users WHERE email = ?;`,
-        email
-      )) as RowDataPacket[];
+      // all schema data is on table saveddb in the SaveData Column.
+      const saveSchemaQuery: string =
+        'UPDATE saveddb SET SaveData = ? WHERE email = ? AND SaveName = ?'; // update existing with schema
+      const values = [schema, email, SaveName];
 
-      console.log('=======');
-      console.log('user[0][0]');
-      console.log('=======');
-      // i just discovered an issue where multiple users are being creating
-      console.log(user[0][0]);
-
-      // I've practiced finding a user. time to get into the MEAT
-      const dataQueryThree: string = 'UPDATE saveddb SET SaveData = ?'; // update existing with schema
-      const values = [email, SaveName, schema, DataType];
-
-      const dataSlot = (await pool.query(dataQueryThree, values)) as RowDataPacket[];
-      console.log('dataslot');
-      console.log(dataSlot[0]);
-
+      const dataSlot = (await pool.query(saveSchemaQuery, values)) as RowDataPacket[];
+      res.locals.message = 'Schema Save Completed';
       return next();
-    } catch {
-      console.log('uh oh our dataquery might have failed');
+    } catch (err) {
+      return next({
+        log: 'An error occured in saveController.save',
+        message: 'something went wrong in the save schema process',
+        status: 400,
+      });
     }
-    try {
-    } catch {}
-    //check, and see if there is a database saved under the current name.
-    //if true -you updated the value to the new schema passed in
-    //else false - create a new database save in the mySQL saveddb table
-    //will need to request current database selected from front end (by email/name)
-    //will update the query with the current state of the database
   },
 
   //delete current save
   delete: async (req: Request, res: Response, next: NextFunction) => {
     log.info("[deleteCtrl - deleteSchema] Begining to deleteing user's schema...");
-    //will need to request current database selected from the front end (by email/name)
-    //include an initial error for no database selected
+
     const { email, SaveName } = req.body;
     console.log(email, SaveName);
-    pool.query(`DELETE FROM savedb WHERE email = ? AND SaveName = ?`, [email, SaveName]);
-    return next();
+    try {
+      const deleteSaveQuery: string = `DELETE FROM saveddb WHERE email = ? AND SaveName = ?`;
+      const values = [email, SaveName];
+      await pool.query(deleteSaveQuery, values);
+      res.locals.message = 'Deletion Completed';
+      return next();
+    } catch (err) {
+      return next({
+        log: 'An error occured in saveController.delete',
+        message: 'something went wrong in the deletion process',
+        status: 400,
+      });
+    }
   },
 
   //load current save
@@ -110,30 +126,43 @@ const saveController = {
     log.info("[loadCtrl - loadSchema] Begining to load user's schema...");
     const { email, SaveName } = req.body;
     console.log(email, SaveName);
-    const loadData = pool.query(`SELECT * WHERE email = ? AND SaveName = ?`, [
-      email,
-      SaveName,
-    ]);
-    console.log(loadData);
-    res.locals.loadData = loadData;
-    return next();
+
+    try {
+      const loadQuery: string = `SELECT SaveData FROM saveddb WHERE email = ? AND SaveName = ?`;
+      const values = [email, SaveName];
+
+      const loadData = (await pool.query(loadQuery, values)) as RowDataPacket[];
+      console.log(loadData[0][0].SaveData);
+      res.locals.data = loadData[0][0].SaveData;
+      return next();
+    } catch (err) {
+      return next({
+        log: 'An error occured in saveController.load',
+        message: 'something went wrong while trying to load',
+        status: 400,
+      });
+    }
     // will need to request current database selected from front end (by email/name)
     //include an initial error for no database selected
   },
   //create new save
   newSave: async (req: Request, res: Response, next: NextFunction) => {
     log.info("[saveCtrl - newSave] Begining to load user's schema...");
-    const { email, SaveName, DataType } = req.body;
-    console.log(email, SaveName, DataType);
+    const { email, SaveName } = req.body;
+    console.log(email, SaveName);
 
-    const newSaveQuery = 'INSERT INTO saveddb(email,SaveName,DataType) VALUES (?,?,?)';
-    const values = [email, SaveName, DataType];
+    const newSaveQuery = 'INSERT INTO saveddb(email,SaveName,SaveData) VALUES (?,?,?)';
+    const values = [email, SaveName, {}];
     try {
       const saveData = await pool.query(newSaveQuery, values);
       console.log(saveData);
       return next();
     } catch (err) {
-      return next(err);
+      return next({
+        log: 'An error occured in saveController.newSave',
+        message: 'something went wrong in the creation process',
+        status: 400,
+      });
     }
   },
   //update a saves name in the database
@@ -159,7 +188,11 @@ const saveController = {
       await pool.query(renameSaveQuery, values);
       return next();
     } catch (err) {
-      return next(err);
+      return next({
+        log: 'An error occured in saveController.renameSave',
+        message: 'something went wrong in the deletion process',
+        status: 400,
+      });
     }
   },
 
@@ -171,8 +204,8 @@ const saveController = {
       const getSavesQuery: string = 'SELECT SaveName FROM saveddb WHERE email =?';
       const values = [email];
       const allSaves = await pool.query(getSavesQuery, values);
-      console.log(allSaves);
-      res.locals.saves = allSaves;
+      console.log(allSaves[0]);
+      res.locals.data = allSaves[0];
       return next();
     } catch (err) {
       return next(err);
@@ -180,84 +213,3 @@ const saveController = {
   },
 };
 export default saveController;
-
-//The original save system relied on using a single entry in a data column on each user.
-// I have added a new primary->foreighnkey pair to have have a seperate table that can infinitely grow
-// Each table will end up having to have a name, and data, and possibly type.
-//I reccomend mySQL workbench plus the google doc for accessing this information.
-
-/*
-// Save currentSchema into database
-export const saveSchema: RequestHandler = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  log.info("[userCtrl - saveSchema] Saving user's schema...");
-  const { schema } = req.body;
-  const { email } = req.session;
-
-  if (typeof email !== 'string' || typeof schema !== 'string') {
-    return res.status(400).json({ error: 'User data must be strings' });
-  }
-
-  const updateColQuery: string = `UPDATE users SET dbs = ? WHERE email = ?;`;
-  try {
-    await pool.query(updateColQuery, [schema, email]);
-    log.info('[userCtrl - saveSchema] New schema saved successfully');
-
-    return next();
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      const error: GlobalError = {
-        log: '[userCtrl - saveSchema] Failed to save new schema: ' + err.message,
-        status: 500,
-        message: 'Failed to save new schema',
-      };
-
-      return next(error);
-    } else {
-      return next(err);
-    }
-  }
-};
-
-
-// Retrieve saved schema
-export const retrieveSchema: RequestHandler = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  log.info("[userCtrl - retrvSchema] Retrieving user's saved schema");
-
-  try {
-    const { email } = req.session;
-    const updateColQuery: string = `SELECT dbs FROM users WHERE email = ?;`;
-    const data = (await pool.query(updateColQuery, [email])) as RowDataPacket[];
-
-    if (data[0][0] && data[0][0].dbs) {
-      res.locals.data = data[0][0].dbs;
-
-      return res.status(200).json(res.locals.data);
-    } else {
-      return res.sendStatus(204);
-    }
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      const error: GlobalError = {
-        log: '[userCtrl - retrvSchema] Failed to retrieve saved schema: ' + err.message,
-        status: 500,
-        message: 'Failed to retrieve saved schema',
-      };
-
-      return next(error);
-    } else {
-      return next(err);
-    }
-  }
-};
-
-
-
-*/
